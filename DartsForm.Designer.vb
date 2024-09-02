3855b36c29524c825caa1674c5801b9026618411
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-09-02 14:03:44 -0600

    Exit Program Added Tooltip added

--- DartsForm.Designer.vb
+++ DartsForm.Designer.vb
@@ -22,11 +22,16 @@ Partial Class DartsForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DartsForm))
         Me.DartPictureBox = New System.Windows.Forms.PictureBox()
         Me.ThrowButton = New System.Windows.Forms.Button()
         Me.ResetButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         CType(Me.DartPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DartPictureBox
@@ -40,6 +45,7 @@ Partial Class DartsForm
         Me.DartPictureBox.Size = New System.Drawing.Size(1061, 985)
         Me.DartPictureBox.TabIndex = 0
         Me.DartPictureBox.TabStop = False
+        Me.ToolTip1.SetToolTip(Me.DartPictureBox, "Displays the board for the dart game.")
         '
         'ThrowButton
         '
@@ -49,6 +55,7 @@ Partial Class DartsForm
         Me.ThrowButton.Size = New System.Drawing.Size(144, 84)
         Me.ThrowButton.TabIndex = 1
         Me.ThrowButton.Text = "Throw"
+        Me.ToolTip1.SetToolTip(Me.ThrowButton, "Throws 3 aleatory ""darts"" to the net and saves the coordinates of this points.")
         Me.ThrowButton.UseVisualStyleBackColor = True
         '
         'ResetButton
@@ -59,31 +66,47 @@ Partial Class DartsForm
         Me.ResetButton.Size = New System.Drawing.Size(144, 84)
         Me.ResetButton.TabIndex = 2
         Me.ResetButton.Text = "Reset"
+        Me.ToolTip1.SetToolTip(Me.ResetButton, "Resets the program to make a new game.")
         Me.ResetButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ExitButton.Location = New System.Drawing.Point(1081, 732)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(144, 84)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Closes the program.")
         Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(8, 8)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
         '
         'DartsForm
         '
+        Me.AcceptButton = Me.ThrowButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
         Me.ClientSize = New System.Drawing.Size(1230, 1009)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ResetButton)
         Me.Controls.Add(Me.ThrowButton)
         Me.Controls.Add(Me.DartPictureBox)
         Me.Name = "DartsForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "Darts Game"
+        Me.ToolTip1.SetToolTip(Me, resources.GetString("$this.ToolTip"))
         CType(Me.DartPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -92,4 +115,6 @@ Partial Class DartsForm
     Friend WithEvents ThrowButton As Button
     Friend WithEvents ResetButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- DartsForm.vb
+++ DartsForm.vb
@@ -2,4 +2,10 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+
+    End Sub
+
 End Class
