94fef5f16a456e9d50d4406fb540b41abb33d0af
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-09-02 18:29:57 -0600

    Previous resource deleted

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property Dart_Net() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Dart_Net", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property New_Dart_Board() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("New_Dart_Board", resourceCulture)
