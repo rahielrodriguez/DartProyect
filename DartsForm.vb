a0e3684ed9174f07370992437e6ace83c6466fb0
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-09-10 09:32:46 -0600

    Comments

--- DartsForm.vb
+++ DartsForm.vb
@@ -13,23 +13,29 @@ Imports System.Text
 Public Class DartsForm
 
     Sub DrawCircle()
+        'Set all the parameters for graphics to be ready to draw
         Dim draw As Pen
         draw = New Pen(Drawing.Color.Blue, 3)
         Dim myGraphics As Graphics = DartPictureBox.CreateGraphics
+        'Variable made to keep track of the dart number for each 3
         Dim dartsThrow As Integer
         Dim file As System.IO.StreamWriter
         dartsThrow = 0
         Randomize()
 
+        'Generates a loop for 3 interations to create all 3 darts
         For i = 1 To 3
+            'Generates the number of the coordinates for the darts and usses a number that covers most of the picture box
             Dim xAxis = 325 * Rnd()
             Dim yAxis = 325 * Rnd()
 
+            'generates all darts and keep tracks from 1 to 3
             dartsThrow += 1
             myGraphics.DrawEllipse(draw, xAxis, yAxis, 5, 5)
 
-
-            File = My.Computer.FileSystem.OpenTextFileWriter("Darts Coordinates.txt", True)
+            'opens a text file with the selected name
+            'writes a line with the number of the dart and the coordinates
+            file = My.Computer.FileSystem.OpenTextFileWriter("Darts Coordinates.txt", True)
             file.WriteLine($"{dartsThrow}. X = {xAxis} , Y = {yAxis}{vbNewLine}")
             file.Close()
         Next
@@ -43,6 +49,7 @@ Public Class DartsForm
         Dim myGraphics As Graphics = DartPictureBox.CreateGraphics
 
         For i = 1 To 3
+            'uses the text box coordinates to replay a dart.
             Dim xAxis = CInt(XTextBox.Text)
             Dim yAxis = CInt(YTextBox.Text)
 
